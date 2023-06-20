f0214fff5a0cd22f2de3d37ff429281200b51ef3
martinapovk <91698417+martinapovk@users.noreply.github.com>
2023-06-20 12:17:29 +0000

    Deploying to gh-pages from @ martinapovk/goit-js-hw-09@e074c72d608ceda635fb2e5709defd5fc0079f10 🚀

---
+++ 01-color-switcher.aa0a5133.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("body"),e=document.querySelector("button[data-start]"),d=document.querySelector("button[data-stop]");let a=null;d.disabled=!0,e.addEventListener("click",(function(){e.disabled=!0,d.disabled=!1,a=setInterval((()=>{t.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3)})),d.addEventListener("click",(function(){e.disabled=!1,d.disabled=!0,clearInterval(a)}));
+//# sourceMappingURL=01-color-switcher.aa0a5133.js.map
