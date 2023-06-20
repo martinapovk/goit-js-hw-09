8d14118a824f4d94d997bb912d84cb1dea7f6530
martinapovk <martinacskapok@gmail.com>
2023-06-20 03:09:00 +0300

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -10,28 +10,25 @@ formRef.addEventListener('submit', formSubmitHandle);
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
 
-  const promise = new Promise((resolve, reject) => {
+  return new Promise((resolve, reject) => {
     setTimeout(() => {
       if (shouldResolve) {
-        resolve(`Fulfill promise ${position + 1} in ${delay}ms`);
+        resolve(`✅ Fulfill promise ${position + 1} in ${delay}ms`);
       } else {
-        reject(`Reject promise ${position + 1} in ${delay}ms`);
+        reject(`❌ Reject promise ${position + 1} in ${delay}ms`);
       }
     }, delay);
   });
-
-  promise
-    .then(res => Notiflix.Notify.success(res))
-    .catch(rej => Notiflix.Notify.failure(rej));
 }
 
 function formSubmitHandle(evt) {
   evt.preventDefault();
 
   for (let i = 0; i < amountInputRef.value; i += 1) {
-    createPromise(
-      i,
-      Number(delayInputRef.value) + i * Number(stepInputRef.value)
-    );
+    const delay = Number(delayInputRef.value) + i * Number(stepInputRef.value);
+
+    createPromise(i, delay)
+      .then(res => Notiflix.Notify.success(res))
+      .catch(rej => Notiflix.Notify.failure(rej));
   }
 }
